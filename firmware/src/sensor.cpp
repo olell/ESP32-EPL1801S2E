1eaa4238c28cf11aff7b5f6a6d76623fea08da96
olel <olel@olel.de>
2023-05-01 18:01:08 +0200

    Add paper and head sensor read

--- firmware/src/sensor.cpp
+++ firmware/src/sensor.cpp
@@ -16,3 +16,17 @@ void sensorInit() {
     pinMode(PIN_SENSOR_HEAD, INPUT);
     pinMode(PIN_SENSOR_PAPER, INPUT);
 }
+
+/**
+ * Read paper sensor state
+ *
+ * @returns 0 -> head up; 1 -> head down
+ */
+uint8_t sensorReadPaper() { return digitalRead(PIN_SENSOR_PAPER); }
+
+/**
+ * Read head sensor state
+ *
+ * @returns 0 -> paper; 1 -> no paper
+ */
+uint8_t sensorReadHead() { return digitalRead(PIN_SENSOR_HEAD); }
