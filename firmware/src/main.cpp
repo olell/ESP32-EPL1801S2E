ac16e81bcc48cc5c11f18c562f4eedf5a66bcd6b
olel <olel@olel.de>
2023-05-19 17:23:13 +0200

    Cleanup

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,17 +1,5 @@
-/**
- * ESP32 Driver for EPL1801S2E
- * Main file
- *
- * (C) 2023 Ole Lange
- */
-
 #include <Arduino.h>
-#include <driver/motor.h>
-#include <driver/sensor.h>
 
-void setup() {
-    motorInit();
-    sensorInit();
-}
+void setup() {}
 
 void loop() {}
